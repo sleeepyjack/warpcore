44bcf95ba49889d0b985fc619cd311e7695a0ce6
Daniel Jünger <juenger@uni-mainz.de>
2020-03-11 00:34:52 +0100

    Added a script for generating include/primes.hpp. Primes are now more finely grained.

--- include/single_value_hash_table.cuh
+++ include/single_value_hash_table.cuh
@@ -117,7 +117,7 @@ public:
         key_type seed = defaults::seed<key_type>(),
         bool no_init = false) noexcept :
         status_(nullptr),
-        table_(get_valid_capacity(min_capacity, cg_size())),
+        table_(detail::get_valid_capacity(min_capacity, cg_size())),
         temp_(TempMemoryBytes / sizeof(index_type)),
         seed_(seed),
         is_initialized_(false),
@@ -206,7 +206,6 @@ public:
 
         value_type * value_ptr =
             insert_impl(key_in, status, group, probing_length);
-
 
         if(group.thread_rank() == 0 && value_ptr != nullptr)
         {
